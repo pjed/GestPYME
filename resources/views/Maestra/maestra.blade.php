00b919a0bebd57bc4c5ec01dbcfeacba5d16e219
DarkS <DarkS@SOBREMESA>
2020-08-12 10:10:03 +0200

    Pagina index finalizada

--- resources/views/Maestra/maestra.blade.php
+++ resources/views/Maestra/maestra.blade.php
@@ -36,82 +36,180 @@
                         <img class="logotipo" src="{{asset ('images/logo.jpg')}}" alt="logotipo">
                     </a>
                 </div>
-                <div class="container col-6">
+                <div class="container col-7">
                     <div class="row h-100 justify-content-center align-items-center">
                         <p class="titulo_app">GestPYME</p>
                     </div>
                 </div>
-                <div class="container col-3">
+                <div class="container col-2">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#registroUsuario">
+                        Registrarse
+                    </button>
 
+                    <!-- Modal -->
+                    <div class="modal fade" id="registroUsuario" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Registro Usuario</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <h6 class="modal-title text-center" id="exampleModalLabel">Rellene los campos</h6>
+                                    <br>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">DNI: </span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="DNI" aria-label="DNI" aria-describedby="basic-addon1">
+                                    </div>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">Nombre: </span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
+                                    </div>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">Apellidos: </span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Apellidos" aria-label="Apellidos" aria-describedby="basic-addon1">
+                                    </div>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">Dirección: </span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Dirección" aria-label="Dirección" aria-describedby="basic-addon1">
+                                    </div>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">Teléfono: </span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Teléfono" aria-label="Teléfono" aria-describedby="basic-addon1">
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                                    <button type="button" class="btn btn-success">Aceptar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-light" data-toggle="modal" data-target="#iniciarSesion">
+                        Iniciar Sesión
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="iniciarSesion" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Iniciar Sesión</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">Correo Electrónico: </span>
+                                        </div>
+                                        <input type="text" class="form-control" placeholder="Correo Electrónico" aria-label="Correo Electrónico" aria-describedby="basic-addon1">
+                                    </div>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text" id="basic-addon1">Contraseña: </span>
+                                        </div>
+                                        <input type="password" class="form-control" placeholder="Contraseña" aria-label="Contraseña" aria-describedby="basic-addon1">
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                                    <button type="button" class="btn btn-success">Aceptar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
+        <nav class="navbar navbar-dark bg-primary">
+            <a class="navbar-brand" href="#">Inicio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pedir cita online</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Lista de Precios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ubicación</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <main>
-            <div class="col-2">
-                <img class="fondo_barber" src="{{asset ('images/fondo_barber.png')}}">
-            </div>
-            
-            <div class="col-10">
-                
-            </div>
+            @yield('contenido')
         </main>
         <footer class="footer bg-dark container-fluid fixed-bottom">  
             <nav class="nav">
                 <div class="col-1 container">
                     <div class="row h-100 justify-content-center align-items-center">
-                        <a class="col nav-link" href="https://europa.eu/european-union/index_es">
+                        <a class="col nav-link" href="https://wahlspain.es/">
                             <img class="borde_logo" src="{{asset ('images/wahl_logo.png')}}" alt="wahl logo">
                         </a>
                     </div>
                 </div>
                 <div class="col-1 container">
                     <div class="row h-100 justify-content-center align-items-center">
-                        <a class="col nav-link" href="http://http://www.educa.jccm.es/es/fpclm/fp-dual">
+                        <a class="col nav-link" href="https://www.wella.com/professional/es-ES/home">
                             <img class="borde_logo" src="{{asset ('images/wella_logo.png')}}" alt="wella logo">
                         </a>
                     </div>
                 </div>
                 <div class="col-1 container">
                     <div class="row h-100 justify-content-center align-items-center">
-                        <a class="col nav-link" href="http://www.cifpvirgendegracia.com/">
+                        <a class="col nav-link" href="#">
                             <img class="borde_logo" src="{{asset ('images/barber_logo.png')}}" alt="barber logo">
                         </a>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-2 container">
                     <div class="row h-100 justify-content-center align-items-center">
-                        <p class="text-center">Pedro Javier Espinosa Duque</p>
+                        <h6>Dirección</h6>
+                        <p class="text-center">Alameda 5 Puertollano (Ciudad Real)</p>
                     </div>
                 </div>
                 <div class="col-2 container">
                     <div class="row h-100 justify-content-center align-items-center">
-                        <p class="text-center">2020 - © Todos los Derechos Reservados</p>
+                        <p class="text-center">
+                        <ul>
+                            <li>Teléfono: 926425878</li>
+                            <li>Teléfono: 625526154</li>
+                        </ul>
+                        </p>
                     </div>
                 </div>
-                <div class="col-1">
+                <div class="col-3 container">
                     <div class="row h-100 justify-content-center align-items-center">
-                        <button type="button" class="btn" id="info"  data-toggle="modal" data-target="#exampleModal">
-                        </button>
+                        <p class="text-center">Pedro Javier Espinosa Duque</p>
+                        <p class="text-center">2020 - © Todos los Derechos Reservados</p>
                     </div>
-                    <!-- Modal -->
-                    <div class="modal  fade" id="exampleModal" tabindex="-1" role="dialog"  aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-body">
-                                    <h2 class="text-center">Derechos de los iconos</h2>
-                                    <h3 class="text-center">Añadir:</h3><a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-                                    <h3 class="text-center">User:</h3><a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-                                    <h3 class="text-center">Delete: </h3><a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-                                    <h3 class="text-center">Confirm:</h3><a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-                                    <h3 class="text-center">Search:</h3><a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-                                    <h3 class="text-center">Edit:</h3><a href="https://www.flaticon.com/authors/kiranshastry" title="Kiranshastry">Kiranshastry</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-                                    <h3 class="text-center">Logout: </h3><a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-                                    <h3 class="text-center">Ticket:</h3><a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-                                    <h3 class="text-center">Informacion</h3><a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>  
                 </div>
             </nav>
         </footer>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,20 @@
 @extends('maestra.maestra')
+@section('contenido')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" charset="UTF-8" content="width=device-width, initial-scale=1">
+        <title>GestPYME</title>
+    </head>
+    <body>
+        <div class="contenedor">
+            <h1>Bievenidos a Peluquería el Paisano</h1>
+            <br>
+            <br>
+            <p>En nuestra peluquería esto es un proyecto de prueba para ver como sale la cosa y si funciona lo pondremos en marcha</p>
+        </div>
+        <video src="video/barber.mp4" autoplay loop muted class="video"></video>
+    </body>
+</html>
+
+@stop
